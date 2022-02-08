f36d1af60b3295f665117bb62953c255b3a2fdca
Tien <viettien1602@gmail.com>
2022-02-08 22:06:35 +0700

    add .env into .gitignore

--- validators/testMessage.js
+++ validators/testMessage.js
@@ -1,5 +1,5 @@
 function testWish(wish) {
-    let invalidPattern = /.*d+m+|đm+|địt+|d+i+t+|mọe|đéo+|méo+|cặc|lồn|c+a+c+|l+o+n+|đít+|boogers|snot|poop|shucks|fuck|pussy|đĩ|ngu+|đụ|du+|di+|v+l+|v+c+l+|cút|phắn|xéo|cứt|chó.*/;
+    let invalidPattern = /.*d+m+|đm+|địt+|d+i+t+|mọe|đéo+|méo+|cặc|lồn|c+a+c+|l+o+n+|đít+|boogers|snot|poop|shucks|fuck|pussy|đĩ|ngu+|đụ|di+|v+l+|v+c+l+|cút|phắn|xéo|cứt|chó.*/i;
     return !invalidPattern.test(wish);
 }
 
